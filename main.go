46209d683b48a03a6d11a8c430a4756e3dd2c936
Keshav Nanda <keshav.nanda@gmail.com>
2026-07-01 21:37:41 +0400

    basic template of proj

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+const BUCKETS = 100
+
+var arr [BUCKETS]int
+
+// type <name of struct> struct
+type Entry struct {
+	key string
+	val int
+}
+
+func PUT(key int, value int) {
+
+}
+
+func GET(key int) {
+
+}
+
+func DELETE(key int) {
+
+}
+func main() {
+	fmt.Println("hello world !")
+}
