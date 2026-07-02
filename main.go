fa215acab14d20ceb9ae2a161b48cf907ff38a59
Keshav Nanda <keshav.nanda@gmail.com>
2026-07-02 13:47:54 +0400

    write resize function that allocates new buckets based on load factor

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"hash/fnv"
 )
 
-const BUCKET_SIZE = 100
+var BUCKET_SIZE = 100
+var ENTRIES = 0
 
 // type <name of struct> struct
 type Entry struct {
@@ -13,7 +14,7 @@ type Entry struct {
 	val string
 }
 
-var table [BUCKET_SIZE][]Entry
+var table = make([][]Entry, BUCKET_SIZE)
 
 func hash(key string) uint64 {
 	hash_obj := fnv.New64a()
@@ -23,7 +24,7 @@ func hash(key string) uint64 {
 
 func PUT(key1 string, value string) {
 	hashed_key := hash(key1)
-	bucket := hashed_key % BUCKET_SIZE
+	bucket := int(hashed_key % uint64(BUCKET_SIZE))
 	found := 1
 	for index, entry := range table[bucket] {
 		if entry.key == key1 {
@@ -36,13 +37,15 @@ func PUT(key1 string, value string) {
 		fmt.Println("successfully updated key present in bucket !")
 	} else if found == 1 {
 		table[bucket] = append(table[bucket], Entry{key: key1, val: value})
+		ENTRIES++
 		fmt.Println("successfully added key-value to bucket!")
+		resize()
 	}
 }
 
 func GET(key1 string) {
 	hashed_key := hash(key1)
-	bucket := hashed_key % BUCKET_SIZE
+	bucket := int(hashed_key % uint64(BUCKET_SIZE))
 
 	for index, entry := range table[bucket] {
 		if entry.key == key1 {
@@ -53,13 +56,36 @@ func GET(key1 string) {
 
 func DELETE(key1 string) {
 	hashed_key := hash(key1)
-	bucket := hashed_key % BUCKET_SIZE
-
+	bucket := int(hashed_key % uint64(BUCKET_SIZE))
+	found := 1
 	for index, entry := range table[bucket] {
 		if entry.key == key1 {
 			table[bucket] = append(table[bucket][:index], table[bucket][index+1:]...)
+			found = 0
+			ENTRIES--
 			fmt.Printf("Value is = %s\n", table[bucket][index].val)
 		}
+	}
+	if found == 0 {
+		fmt.Println("Element successfully deleted!")
+	} else {
+		fmt.Println("Element not found")
+	}
+}
+
+func resize() {
+	//every entry in a bucket has to be rehashed and appended to new table
+	if float64(ENTRIES)/float64(BUCKET_SIZE) > 0.75 {
+		NEW_BUCKET_SIZE := 2 * BUCKET_SIZE
+		new_table := make([][]Entry, NEW_BUCKET_SIZE)
+		for _, bucket := range table {
+			for _, entry := range bucket {
+				new_bucket := int(hash(entry.key) % uint64(NEW_BUCKET_SIZE)) //bucket shd always be int
+				new_table[new_bucket] = append(new_table[new_bucket], entry)
+			}
+		}
+		table = new_table
+		BUCKET_SIZE = NEW_BUCKET_SIZE
 	}
 }
 func main() {
